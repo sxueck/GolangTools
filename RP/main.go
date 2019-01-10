fe915b4310f823e2016a5afab27185081fc9f83d
sxuecks <sxueck@outlook.com>
2019-01-10 18:14:40 +0800

    修复算法错误问题

--- RP/main.go
+++ RP/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math/rand"
 	"strconv"
 	"time"
@@ -11,33 +12,35 @@ import (
 )
 
 func main() {
-
-	fmt.Println(goodLucky())
-
 	var people [10000]float64
 	var luckyValue [10000]int
 
+	fmt.Println("开始模拟, 共10轮")
 	for i := 0; i < 10; i++ {
-		fmt.Printf("第%d轮模拟开始", i+1)
+		fmt.Printf("第%d轮模拟开始\n", i+1)
 		for count := 3360; count >= 0; count-- {
 			for j := 0; j < 10000; j++ {
 				if result := goodLucky(); result > 0 {
+					//fmt.Println(result)
 					//so lucky
 					people[j] += result
 
 					//count lucky
 					luckyValue[j]++
 				} else {
+					//fmt.Println(result)
 					//so bad
 					people[j] += result
 					//but you work hard
 					people[j]++
 				}
 			}
-			sheetName := string(i) + "round"
-			savaData(sheetName, people, luckyValue)
 		}
+		sheetName := strconv.Itoa(i) + "round"
+		savaData(sheetName, people, luckyValue)
 	}
+	fmt.Println("模拟结束")
+	fmt.Scan()
 }
 
 func floatToString(num float64) string {
@@ -48,16 +51,24 @@ func goodLucky() float64 {
 	var countValue float64
 	rng := rand.New(mt19937.New())
 
-	for i := 0; i > 10; i++ {
-		rng.Seed(time.Now().UnixNano())
+	for i := 0; i < 10; i++ {
+		rng.Seed(time.Now().UnixNano() + int64(i))
 		countValue += rng.NormFloat64()
 	}
 	return countValue
 }
 
 func savaData(sheetName string, people [10000]float64, luckyValue [10000]int) {
-	file, _ := xlsx.OpenFile("result.xlsx")
-	sheet, _ := file.AddSheet(sheetName)
+	file, err := xlsx.OpenFile("result.xlsx")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sheet, err := file.AddSheet(sheetName)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	for i := 0; i < 10000; i++ {
 		row := sheet.AddRow()
@@ -66,5 +77,5 @@ func savaData(sheetName string, people [10000]float64, luckyValue [10000]int) {
 		cell = row.AddCell()
 		cell.Value = strconv.Itoa(luckyValue[i])
 	}
-
+	file.Save("result.xlsx")
 }
