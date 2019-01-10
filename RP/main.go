af8b704f439fd17c97907f8ca3d163aa8ade091c
sxuecks <sxueck@outlook.com>
2019-01-10 21:40:57 +0800

    添加多线程模式

--- RP/main.go
+++ RP/main.go
@@ -12,57 +12,82 @@ import (
 )
 
 func main() {
+	ch := make(chan bool)
+	for i := 0; i < 9; i++ {
+		if i != 8 {
+			go goSimulation(i, false, nil)
+		} else {
+			go goSimulation(i, true, ch)
+		}
+	}
+	<-ch
+	fmt.Println("模拟结束")
+	fmt.Scan()
+}
+
+func goSimulation(i int, checkEndChannel bool, ch chan bool) {
 	var people [10000]float64
 	var luckyValue [10000]int
 
-	fmt.Println("开始模拟, 共10轮")
-	for i := 0; i < 10; i++ {
-		fmt.Printf("第%d轮模拟开始\n", i+1)
-		for count := 3360; count >= 0; count-- {
-			for j := 0; j < 10000; j++ {
-				if result := goodLucky(); result > 0 {
-					//fmt.Println(result)
-					//so lucky
-					people[j] += result
+	fmt.Printf("线程%d启动模拟\n", i+1)
+	for count := 3360; count >= 0; count-- {
+		for j := 0; j < 10000; j++ {
+			if result := goodLucky(i); result > 0 {
+				//fmt.Println(result)
+				//so lucky
+				people[j] += result
 
-					//count lucky
-					luckyValue[j]++
-				} else {
-					//fmt.Println(result)
-					//so bad
-					people[j] += result
-					//but you work hard
-					people[j]++
-				}
+				//count lucky
+				luckyValue[j]++
+			} else {
+				//fmt.Println(result)
+				//so bad
+				people[j] += result
+				//but you work hard
+				people[j]++
 			}
 		}
-		sheetName := strconv.Itoa(i) + "round"
-		savaData(sheetName, people, luckyValue)
 	}
-	fmt.Println("模拟结束")
-	fmt.Scan()
+	fmt.Printf("线程%d模拟结束,开始导出数据\n", i+1)
+	sheetName := strconv.Itoa(i) + "round"
+
+	for {
+		if savaData(sheetName, people, luckyValue) == false {
+			//如果文件被占用,等待下一个周期
+			time.Sleep(time.Duration(3) * time.Second)
+		} else {
+			break
+		}
+	}
+
+	fmt.Printf("线程%d导出数据完成\n", i+1)
+	if checkEndChannel == true {
+		ch <- true
+	}
 }
 
 func floatToString(num float64) string {
 	return strconv.FormatFloat(num, 'f', 6, 64)
 }
 
-func goodLucky() float64 {
+func goodLucky(salt int) float64 {
 	var countValue float64
 	rng := rand.New(mt19937.New())
 
 	for i := 0; i < 10; i++ {
-		rng.Seed(time.Now().UnixNano() + int64(i))
+		rng.Seed(time.Now().UnixNano() + int64(salt))
 		countValue += rng.NormFloat64()
 	}
 	return countValue
 }
 
-func savaData(sheetName string, people [10000]float64, luckyValue [10000]int) {
+func savaData(sheetName string, people [10000]float64, luckyValue [10000]int) bool {
 	file, err := xlsx.OpenFile("result.xlsx")
 
 	if err != nil {
-		log.Fatal(err)
+		//可能是文件被占用
+		fmt.Println(err)
+		return false
 	}
 
 	sheet, err := file.AddSheet(sheetName)
@@ -78,4 +103,5 @@ func savaData(sheetName string, people [10000]float64, luckyValue [10000]int) {
 		cell.Value = strconv.Itoa(luckyValue[i])
 	}
 	file.Save("result.xlsx")
+	return true
 }
